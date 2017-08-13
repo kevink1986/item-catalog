bbc9df4e147560a356d31e52d99d851029c18bed
Kevin Kriek <kevinkriek@gmail.com>
2017-08-13 12:15:45 +0200

    Added a category and an item page.

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
 app = Flask(__name__)
 
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 
@@ -29,14 +29,21 @@ session = DBSession()
 def showCatalog():
     #return "This is a catalog application"
     categories = session.query(Category).order_by(asc(Category.name)).all()
-    items = session.query(Item).order_by(asc(Item.name)).all()
+    items = session.query(Item).order_by(desc(Item.id)).limit(8)
     return render_template('catalog.html', categories=categories, items=items)
 
 #Show a category of catalog items
 @app.route('/catalog/<string:category_name>/')
 @app.route('/catalog/<string:category_name>/items/')
 def showCategory(category_name):
-    return "This is a list of catalog items of category: %s" % category_name
+    categories = session.query(Category).order_by(asc(Category.name)).all()
+    if not any(c.name == category_name for c in categories):
+        return redirect(url_for('showCatalog'))
+    category = session.query(Category).filter_by(name = category_name).one()
+    items = session.query(Item).filter_by(category_id = category.id).all()
+    count = len(items)
+    return render_template('category.html', categories=categories, category=category, items=items, count=count)
+    #return "This is a list of catalog items of category: %s" % category_name
 
 
 #Add a catalog item
@@ -48,7 +55,10 @@ def addItem(category_name, item_name):
 #Show a catalog item
 @app.route('/catalog/<string:category_name>/<string:item_name>/')
 def showItem(category_name, item_name):
-    return "Show catalog item %s from category %s" % (item_name, category_name)
+    category = session.query(Category).filter_by(name = category_name).one()
+    item = session.query(Item).filter_by(name = item_name, category_id = category.id).one()
+    return render_template('item.html', item=item)
+    #return "Show catalog item %s from category %s" % (item_name, category_name)
 
 
 #Edit a catalog item
